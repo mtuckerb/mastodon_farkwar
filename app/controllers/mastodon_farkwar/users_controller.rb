a712014d95ac8947636ae744420ee3d44ff5531c
Tucker Bradford <tucker@tuckerbradford.com>
2019-09-26 06:48:46 +0000

    bug fix for AppSignupService typo

--- app/controllers/mastodon_farkwar/users_controller.rb
+++ app/controllers/mastodon_farkwar/users_controller.rb
@@ -5,7 +5,7 @@ module MastodonFarkwar
     skip_before_action :verify_authenticity_token
   
     def create
-      token = ::ApplicationSignupService.new.call(app, params)
+      token = ::AppSignupService.new.call(app, params)
       user = ::Account.find_by(domain: nil, username: user_params[:username])&.user
       user.confirmed_at = Time.now
       if user.save
